ca380df1956bea7c88db162867141df274ca5117
Daniel Breedeveld <dhbreedeveld@gmail.com>
2018-04-04 01:02:20 +0200

    Updated .gitignore & Changed QR button layout
    Updated to flutter's .gitignore and changed the QR button UI.

--- lib/components/login_button.dart
+++ lib/components/login_button.dart
@@ -11,7 +11,7 @@ class LoginButton extends StatelessWidget {
     return new Container(
       height: 50.0,
       width: double.infinity,
-      margin: new EdgeInsets.all(10.0),
+      margin: new EdgeInsets.only(left: 5.0, right: 5.0, bottom: 0.0),
       child: new FlatButton(
         onPressed: () {
           Navigator.push(
