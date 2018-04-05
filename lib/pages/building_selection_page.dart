32807dde9d9aa20c49d1ce283ff25de5406e59fb
Daniel Breedeveld <dhbreedeveld@gmail.com>
2018-04-05 14:39:23 +0200

    Added listview of rooms, should be buildings only next time

--- lib/pages/building_selection_page.dart
+++ lib/pages/building_selection_page.dart
@@ -1,6 +1,32 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class BuildingSelectionPage extends StatelessWidget {
+class BuildingSelectionPage extends StatefulWidget {
+  @override
+  _BuildingSelectionPage createState() => new _BuildingSelectionPage();
+}
+
+class _BuildingSelectionPage extends State<BuildingSelectionPage> {
+  //Get info from room number (result of QR code scan)
+  String url = "https://ketagenda-199308.appspot.com/api/rooms/";
+  List data;
+  TextEditingController _controllerRoomName;
+  TextEditingController _controllerRoomType;
+  TextEditingController _controllerRoomLocation;
+  TextEditingController _controllerRoomFloor;
+
+  Future getSWData() async {
+    var res = await http
+        .get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    setState(() {
+      var resBody = json.decode(res.body);
+      data = resBody;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -40,20 +66,31 @@ class BuildingSelectionPage extends StatelessWidget {
             ),
           ),
           new Expanded(
-            child: new Material(
-              color: Colors.red,
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  //Add content
-                ],
+            child: new Scaffold(
+              backgroundColor: Colors.redAccent[50],
+              body: new ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return new ListTile(
+                    leading: new CircleAvatar(
+                      child: new Text((data[index]["location"].toString())),
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    title: new Text(data[index]["name"].toString()),
+                    subtitle: new Text(data[index]["type"].toString()),
+                  );
+                },
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getSWData();
   }
 }
